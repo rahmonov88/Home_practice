dded0794c19a4dd0f79b17d42181045fe748b091
hamid <rahmonovx88@gmail.com>
2023-09-23 09:51:35 -0400

    My All commit

--- src/Day_6_Practice_Programming/Taksnumber.java
+++ src/Day_6_Practice_Programming/Taksnumber.java
@@ -1,10 +1,2 @@
 package Day_6_Practice_Programming;
 
-public class Taksnumber {
-    public static void main(String[] args) {
-        int R = 20;
-        int W = -R-- + -R++ + --R * R-- %;
-        System.out.println(R);
-        System.out.println(W);
-    }
-}
